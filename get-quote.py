71e524567378703fb08e0d6fce9175e1350a8b55
Sethu Pavan <60856766+sethupavan12@users.noreply.github.com>
2020-11-15 12:03:17 +0000

    Updated py file.
    Used to give just the same quote every time. Now it is wiser than before

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
+import random
 def main():
-   print("Keep it logically awesome.")
-
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+ f = open("quotes.txt")
+ quotes = f.readlines()
+ quotes=[s.strip() for s in quotes]
+ #print("Keep it logically awesome.")
+ f.close()
+ l=len(quotes)
+ r=random.randint(0,l)
+ print(quotes[r])
 
 if __name__== "__main__":
   main()
